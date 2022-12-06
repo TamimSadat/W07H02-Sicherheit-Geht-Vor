c3bd128f3c8187610706092a8bc102d073e893ae
Said Tamim Sadat <ge87mit@mytum.de>
2022-12-06 22:55:08 +0100

    test

--- src/pgdp/security/SignalPost.java
+++ src/pgdp/security/SignalPost.java
@@ -1,13 +1,43 @@
 package pgdp.security;
 
-public class SignalPost {
+public abstract class SignalPost {
 
 	/**
 	 * Diese Klasse ist nur da, damit keine Buildfails entstehen. Allerdings ist sie
 	 * bei Weitem noch nicht vollständig.
 	 * 
 	 */
+	private int postNumber;
+	private String depiction;//Darstellung des SignalPosts speichert
+	private int level;
 
 	public SignalPost(int postNumber) {
+		this.postNumber = postNumber;
+		this.level = 0;
+		this.depiction = "";
+	}
+
+	public int getPostNumber() {
+		return postNumber;
+	}
+
+	public void setPostNumber(int postNumber) {
+		this.postNumber = postNumber;
+	}
+
+	public String getDepiction() {
+		return depiction;
+	}
+
+	public void setDepiction(String depiction) {
+		this.depiction = depiction;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
 	}
 }
